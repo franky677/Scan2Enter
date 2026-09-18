7508d6089c1d248e6ce5db80b4ef80640c3b2f46
frank <frank@local>
2026-09-18 22:14:41 +0200

    chore: aggiorna versione Android a 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.scan2enter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
